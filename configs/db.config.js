4f9159942825cf5ed7108a7e1c22192af9176f80
Nikesh-kathayat <nikeshsinghkathayat@gmail.com>
2022-09-25 00:30:02 +0530

    initial boilerplate code

---
+++ configs/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST : "localhost",
+    USER : "root",
+    PASSWORD : "niksql@30",
+    DB : "ecom_db",
+    dialect : "mysql",
+    pool :{
+        max :5,
+        min :0,
+        acquire: 30000, //max time in ms that  a pool will try to get connection before throwing an error
+        idle :10000 //maximum time in ms that a connection can be idlebefore beig released
+
+    }
+};
