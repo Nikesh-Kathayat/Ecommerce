8378bcd54850b90b543a08049b610da07a10765e
Nikesh-kathayat <nikeshsinghkathayat@gmail.com>
2022-10-03 17:17:04 +0530

    Login and used async await

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -1,36 +1,51 @@
-const { User, Role, Sequelize, ROLES  } = require("../models");
+const { User, Role, Sequelize, ROLES } = require("../models");
 const bcrypt = require("bcrypt");
+const e = require("express");
 
 
-exports.signup=(req,res)=>{
+exports.signup = async (req, res) => {
 
-    var {userName,email,password,roles}=req.body;
+    var { userName, email, password, roles } = req.body;
 
-    if(!roles || !roles.length){
-        roles=[ROLES[0]];
+    if (!roles || !roles.length) {
+        roles = [ROLES[0]];
     }
 
-    User.create({
-        userName:userName,
-        email:email,
-        password:bcrypt.hashSync(password,8)
+    try {
+        const user = await User.create({ userName, email, password: bcrypt.hashSync(password, 8) });
+        const userRoles = await Role.findAll({ where: { name: { [Sequelize.Op.or]: roles } } });
+        await user.setRoles(userRoles);
+        res.send({ message: "User registered successfully" });
+    } catch (e) {
+        res.status(400).send({ message: e.message || "Something went wrong" })
+    }
+}
+
+exports.signIn = async (req, res) => {
+
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        res.status(400).send({ message: "UserName or Password cannot be empty" });
+    }
+
+    try {
+        var user = await User.findOne({ where: { userName: username } });
+    } catch (e) {
+        return res.status(500).send({ message: e.message });
+    }
+    if (!user) {
+        return res.status(400).send({ message: "user not found" });
+    }
+    var isPasswordValid = bcrypt.compareSync(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.status(401).send({ message: "Invalid Password" });
+    }
+    res.send({
+        id: user.id,
+        userName: user.userName,
+        email: user.email,
+        roles: user.roles
     })
-    .then(user=>{
-            Role.findAll({
-                where:{
-                    name:{
-                        [Sequelize.Op.or] : roles
-                    }
-                }
-            })
-            .then(roles=>{
-                user.setRoles(roles)
-                .then(()=>{
-                    res.send({message:"User registered successfully"});
-                })
-            })
-    })
-    .catch((err)=>{
-        res.status(500).send({message:"Something went wrong"});
-    }) 
 }
--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -6,4 +6,6 @@ module.exports=function(app){
 
     app.post("/ecomm/api/v1/auth/signup",[signUpValidator.checkDuplicateEmailOrUserName,signUpValidator.checkRolesExists],
     authController.signup);
+
+    app.post("/ecomm/api/v1/auth/signin",authController.signIn);
 }
