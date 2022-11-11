d9eb9abf32dc2071b5a28b9e00dc9cd475bd1e44
Nikesh-kathayat <nikeshsinghkathayat@gmail.com>
2022-11-11 16:24:12 +0530

    updated code

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -1,65 +1,68 @@
-const { User, Role, Sequelize, ROLES } = require("../models");
+const {User, Role , Sequelize, ROLES} = require("../models");
 const bcrypt = require("bcrypt");
-const jwt =require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 
-exports.signup = async (req, res) => {
 
-    var { userName, email, password, roles } = req.body;
+exports.signup= async (req,res)=>{
 
-    if (!roles || !roles.length) {
-        roles = [ROLES[0]];
+    var {userName,email,password,roles}=req.body;
+
+    if(!roles || !roles.length){
+        roles=[ROLES[0]];
     }
 
-    try {
-        const user = await User.create({ userName, email, password: bcrypt.hashSync(password, 8) });
-        const userRoles = await Role.findAll({ where: { name: { [Sequelize.Op.or]: roles } } });
-        await user.setRoles(userRoles);
-        res.send({ message: "User registered successfully" });
-    } catch (e) {
-        res.status(400).send({ message: e.message || "Something went wrong" })
+    try{
+    const user = await User.create({userName,email,password:bcrypt.hashSync(password,8)});
+    const userRoles = await  Role.findAll({where:{name:{[Sequelize.Op.or] : roles}}});
+    console.log(userRoles);
+    await  user.setRoles(userRoles);
+    res.send({message:"User resgistered successfully"});
+    }catch(e){
+        res.status(500).send({message: e.message || "Something went wrong" });
     }
 }
 
-exports.signIn = async (req, res) => {
 
-    const { username, password } = req.body;
+exports.signIn = async (req,res) =>{
 
-    if (!username || !password) {
-        res.status(400).send({ message: "UserName or Password cannot be empty" });
+    const {username,password}= req.body;
+
+    if(!username || !password){
+        res.status(400).send({message:"UserName or Password cannot be empty"});
     }
 
-    try {
-        var user = await User.findOne({ where: { userName: username } });
-    } catch (e) {
-        return res.status(500).send({ message: e.message });
-    }
-    if (!user) {
-        return res.status(400).send({ message: "user not found" });
-    }
-    var isPasswordValid = bcrypt.compareSync(password, user.password);
-    if (!isPasswordValid) {
-        return res.status(401).send({ message: "Invalid Password" });
+    try{
+    var user = await User.findOne({where:{userName:username}});  
+    }   catch(e){
+        return res.status(500).send({message:e.message});
     }
 
-    console.log(user.Id);
+    if(!user){
+        return res.status(400).send({message:"user not found"});
+       }
+  
+    var isPasswordValid= bcrypt.compareSync(password,user.password);
+
+    if(!isPasswordValid){
+        return res.status(401).send({message:"Invalid Password"});
+    }
 
 
-    const token = jwt.sign({ id:user.id }, process.env.SECRET_KEY,{expiresIn:86400});
-    
-    var roles=[];
+      const token=jwt.sign({ id:user.id }, process.env.SECRET_KEY,{expiresIn:86400});
 
-    const allRoles = await user.getRoles();
+      var roles=[];
 
-    allRoles.forEach(role=>{
-        roles.push(role.name);
-    })
+      const allRoles= await user.getRoles();
 
-    res.send({
-        id: user.id,
-        userName: user.userName,
-        email: user.email,
-        roles: roles,
-        accessToken:token
-    })
+      allRoles.forEach(role => {
+          roles.push(role.name);
+      });
+
+    res.send({id:user.id,
+            userName:user.userName,
+            email:user.email,
+            roles:roles,
+            accessToken:token
+        })
 }
--- Routes/product.routes.js
+++ Routes/product.routes.js
@@ -1,27 +1,27 @@
 
-const ProductControllers= require("../Controllers/product.controllers");
-const {requestValidator, authJWT    }=require("../Middlewares");
+const ProductControllers = require("../Controllers/product.controllers");
+const { requestValidator, authJWT } = require("../Middlewares");
 
-module.exports=(app)=>{
+module.exports = (app) => {
 
-    app.post("/ecomm/api/v1/products",[requestValidator.validateProductRequest, authJWT.verifyToken],ProductControllers.create)
- 
-    app.get("/ecomm/api/v1/products",ProductControllers.findAll);
+    app.post("/ecomm/api/v1/products", [requestValidator.validateProductRequest, authJWT.verifyToken], ProductControllers.create)
 
-    app.get("/ecomm/api/v1/products/:id",ProductControllers.findOne);
+    app.get("/ecomm/api/v1/products", ProductControllers.findAll);
 
-    app.put("/ecomm/api/v1/products/:id",[authJWT.verifyToken],ProductControllers.update);
+    app.get("/ecomm/api/v1/products/:id", ProductControllers.findOne);
 
-    app.delete("/ecomm/api/v1/products/:id",[authJWT.verifyToken],ProductControllers.delete);
+    app.put("/ecomm/api/v1/products/:id", [authJWT.verifyToken], ProductControllers.update);
+
+    app.delete("/ecomm/api/v1/products/:id", [authJWT.verifyToken], ProductControllers.delete);
 
     // Routes for getting a list of product under a particular category  
 
-    app.get("/ecomm/api/v1/category/:categoryId/products",requestValidator.validateCategoryPassed,ProductControllers.findProductsUnderCategory);
+    app.get("/ecomm/api/v1/category/:categoryId/products", requestValidator.validateCategoryPassed, ProductControllers.findProductsUnderCategory);
 
     // you want a product with a productId under a category as categoryId 
-    app.get("/ecomm/api/v1/category/:categoryId/products/:productId",requestValidator.validateCategoryAndProductPassed,ProductControllers.findProductUnderCategory);
+    app.get("/ecomm/api/v1/category/:categoryId/products/:productId", requestValidator.validateCategoryAndProductPassed, ProductControllers.findProductUnderCategory);
 
 
 
- 
+
 }    
