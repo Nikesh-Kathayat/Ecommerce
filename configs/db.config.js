566d2a37f6e260accd2621028c3c2f6d678b4350
Nikesh-kathayat <nikeshsinghkathayat@gmail.com>
2022-11-11 13:07:14 +0530

    added db prod configs

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,14 +1,44 @@
 module.exports = {
-    HOST : "localhost",
-    USER : "root",
-    PASSWORD : "niksql@30",
-    DB : "ecom_db",
-    dialect : "mysql",
-    pool :{
-        max :5,
-        min :0,
-        acquire: 30000, //max time in ms that  a pool will try to get connection before throwing an error
-        idle :10000 //maximum time in ms that a connection can be idlebefore beig released
+    development: {
+        HOST: "localhost",
+        USER: "root",
+        PASSWORD: "niksql@30",
+        DB: "ecom_db",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000, //max time in ms that  a pool will try to get connection before throwing an error
+            idle: 10000 //maximum time in ms that a connection can be idlebefore beig released
 
+        }
+    },
+    test: {
+        HOST: "localhost",
+        USER: "root",
+        PASSWORD: "Mohit@19",
+        DB: "ecom_test_db",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acuire: 30000, //max time in ms that a pool will try to get connection before throwing error
+            idle: 10000  // maximum time in ms that a connection can be idle before being released
+        }
+    },
+        production: {
+            HOST: "sql12.freemysqlhosting.net",
+            USER: "sql12552139",
+            PASSWORD: "g8ZP8LS3cN",
+            DB: "sql12552139",
+            dialect: "mysql",
+            pool: {
+                max: 5,
+                min: 0,
+                acquire: 30000, //max time in ms that  a pool will try to get connection before throwing an error
+                idle: 10000 //maximum time in ms that a connection can be idlebefore beig released
+
+            }
+
+        }
     }
-}
--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
-const config= require("../configs/db.config");
 const Sequelize = require("sequelize");
+
+const env = process.env.NODE_ENV || 'development';
+const config = require("../configs/db.config")[env];
 
 const sequelize = new Sequelize(
     config.DB,
